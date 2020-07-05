e33c225a3598289143679ca4ff2a8cffa64664e3
charlyecastro <castroc@uw.edu>
2020-07-04 19:09:03 -0700

    added imports, set up listview for todo list, added icons, added date

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import moment from 'moment'
+import { List, Input, Checkbox, Popconfirm, message, Skeleton, Switch, Avatar } from 'antd';
+import 'antd/dist/antd.css';
+import { DeleteTwoTone } from '@ant-design/icons';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { Search } = Input
+  // var d = ;
+  // console.log()
+  useEffect(() => {
+    fetchAll()
+  }, []);
+
+  const fetchAll = (() => {
+    Axios.get("https://radiant-tundra-36373.herokuapp.com/all")
+      .then(res => {
+        return res.data
+      })
+      .then(data => {
+        let list = Object.entries(data.data)
+        setList(list)
+        console.log(list)
+        setLoading(false)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  });
+
+  const confirm = (e) => {
+    console.log(e);
+    // message.success('Succesfully Deleted');
+  }
+
+  const onChange = (e) => {
+    if (e.target.checked) {
+      message.success('Nice job!', 1);
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+    <h1>Welcome! Here is your list</h1>
+      </header> */}
+      <main>
+        <h1>Today, {moment(new Date()).format('D MMMM')}</h1>
+        {!loading ?
+          <div>
+            <List
+              size="small"
+              dataSource={list}
+              renderItem={item =>
+                <List.Item actions={[<Popconfirm
+                  title="Are you sure delete this task?"
+                  onConfirm={confirm}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <a href="#"><DeleteTwoTone /></a>
+                </Popconfirm>]} ><Checkbox onChange={onChange} checked = {item[1] == "TRUE"}>{item[0]}</Checkbox></List.Item>}
+            />
+          </div>
+          : <Skeleton active >
+          </Skeleton>}
+        <Search
+          placeholder="e.g. Finish the star wars saga"
+          enterButton="Add Task"
+          size="large"
+          onSearch={value => console.log(value)}
+        />
+      </main>
     </div>
   );
 }
