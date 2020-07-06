8ef476b4cb864b65b112c17742c1636ec23a714b
charlyecastro <castroc@uw.edu>
2020-07-06 12:13:52 -0700

    Updates

---
+++ precache-manifest.50c8212c2540590f853ad25ab93da1b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab089c255857a4c96e9bdc3599a5658",
+    "url": "/todo-web/index.html"
+  },
+  {
+    "revision": "dbb86dea02dec24222f0",
+    "url": "/todo-web/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "a590100e23498dff772d",
+    "url": "/todo-web/static/css/main.57570719.chunk.css"
+  },
+  {
+    "revision": "dbb86dea02dec24222f0",
+    "url": "/todo-web/static/js/2.c587c656.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo-web/static/js/2.c587c656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a590100e23498dff772d",
+    "url": "/todo-web/static/js/main.f3d8fd11.chunk.js"
+  },
+  {
+    "revision": "9bee9e87452bf54613f2",
+    "url": "/todo-web/static/js/runtime-main.33a35612.js"
+  }
+]);
