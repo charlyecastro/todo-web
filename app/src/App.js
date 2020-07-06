5d8391198a4b4a21473ea39e44dcdb98d5a6bd81
charlyecastro <castroc@uw.edu>
2020-07-05 17:47:06 -0700

    updated favicon, fixed deleting bug, and added delete load animation

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import moment from 'moment'
-import { List, Input, Skeleton} from 'antd';
+import { List, Input, Skeleton, message } from 'antd';
 import ListItem from './components/listItem'
 import 'antd/dist/antd.css';
 import './App.css';
 
 function App() {
-  const [list, setList] = useState('');
+  const [list, setList] = useState([]);
   const [loading, setLoading] = useState(true);
+  // const [inputValue, setInputValue] = useState(true);
   const { Search } = Input
 
   useEffect(() => {
     fetchAll()
   }, []);
 
-  const fetchAll = (() => {
-    Axios.get("https://radiant-tundra-36373.herokuapp.com/all")
+  const fetchAll = () => {
+    Axios.get(`${process.env.REACT_APP_BASE_URL}/all`)
       .then(res => {
         return res.data
       })
@@ -28,19 +29,61 @@ function App() {
       .catch(err => {
         console.log(err);
       })
-  });
+  };
+
+  const removeListItem = (todo) => {
+    // const updatedList = list.filter(item => item[0] !== todo)
+    // console.log(updatedList)
+    // setList(updatedList)
+    window.location.reload(false);
+  }
+
+  const postItem = (body) => {
+    Axios.post(`${process.env.REACT_APP_BASE_URL}/data`,
+      body,
+      { 'headers': { 'Content-Type': 'application/json' } })
+      .then(res => {
+        return res.data
+      })
+      .then(data => {
+        console.log(data)
+        
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  const addListItem = (todo) => {
+    if (!isBlank(todo)) {
+      let updatedList = [...list]
+      let item = [todo, "FALSE"]
+      updatedList.push(item)
+      console.log(updatedList)
+      setList(updatedList)
+      let body = {}
+      body[todo.trim()] = false
+      postItem(body)
+    } else {
+      message.error("Can not add empty todo")
+    }
+  }
+
+  const isBlank = (str) => {
+    return (!str || /^\s*$/.test(str));
+  }
 
   return (
     <div className="App">
       <main>
-        <h1>Today, {moment(new Date()).format('D MMMM')}</h1>
+        <h1 style={{ width: '100%', textAlign: "left" }}>Today, {moment(new Date()).format('D MMMM')}</h1>
         {!loading ?
           <div>
             <List
               size="small"
               dataSource={list}
               renderItem={item =>
-                <ListItem todo={item[0]} isDone={item[1] === "TRUE"} />}
+                <ListItem todo={item[0]} isDone={item[1] === "TRUE"} removeListItem={removeListItem} />}
             />
           </div>
           : <Skeleton active >
@@ -49,7 +92,9 @@ function App() {
           placeholder="e.g. Finish the star wars saga"
           enterButton="Add Task"
           size="large"
-          onSearch={value => console.log(value)}
+          onSearch={value => {
+            addListItem(value)
+          }}
         />
       </main>
     </div>
--- app/src/components/listItem.js
+++ app/src/components/listItem.js
@@ -1,26 +1,60 @@
 import React, { useState } from 'react';
-import { List, Checkbox, Popconfirm, message } from 'antd';
+import { List, Checkbox, Popconfirm, message, Spin } from 'antd';
 import 'antd/dist/antd.css';
 import { DeleteTwoTone } from '@ant-design/icons';
 import '../App.css';
+import Axios from 'axios';
 
-function ListItem({ todo, isDone }) {
+
+function ListItem({ todo, isDone, removeListItem }) {
     const [isChecked, setChecked] = useState(isDone)
-
+    const [isDeleting, setIsDeleting] = useState(false)
+    
     const confirm = (e) => {
-        console.log(e);
+        onDelete()
     }
 
     const onChange = () => {
         setChecked(!isChecked)
+        let body = {}
+        body[todo] = !isChecked
         if (!isChecked) {
             message.success('Nice job!', 1);
         }
+        Axios.post(`${process.env.REACT_APP_BASE_URL}/data`,
+            body,
+            { 'headers': { 'Content-Type': 'application/json' } })
+            .then(res => {
+                return res.data
+            })
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    const onDelete = () => {
+        setIsDeleting(true)
+        Axios.delete(`${process.env.REACT_APP_BASE_URL}/data/${todo}`)
+            .then(res => {
+                return res.data
+            })
+            .then(data => {
+                console.log(data)
+                removeListItem(todo);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     return (
-        <List.Item actions={[<Popconfirm title="Delete this task?" onConfirm={confirm}okText="Yes" cancelText="No"> <a href="/#"> <DeleteTwoTone /></a></Popconfirm>]} >
-            <Checkbox onChange={onChange} checked={isChecked}>{todo}</Checkbox>
+        <List.Item actions={[<Popconfirm title="Delete this task?" onConfirm={confirm} okText="Yes" cancelText="No"> <a href="/#"> {isDeleting ? <Spin size = {"small"}/> : <DeleteTwoTone />}</a></Popconfirm>]} >
+            <Checkbox onChange={onChange} checked={isChecked}>
+                {todo}
+            </Checkbox>
         </List.Item>
     )
 }
